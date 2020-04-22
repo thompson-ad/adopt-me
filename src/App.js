cf9419b10726442d90b4a5b9c891674ad96e9f6c
Aaron Thompson <thompson_ad@outlook.com>
2020-04-22 08:15:04 +0100

    convert to jsx

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement(
-      "h1",
-      {},
-      "Adopt Me!",
-      React.createElement(Pet, { name: "luna", breed: "havanese" }),
-      React.createElement(Pet, { name: "Pepper", breed: "budgie" }),
-      React.createElement(Pet, { name: "Benson", breed: "boxer" })
-    )
+  return (
+    <div className="">
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default function Pet({ name, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, breed),
-  ]);
+export default function Pet({ name, breed, animal }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
